acea8bb1ec7462d7aa2b65c075ba56e15f4ebfee
pshinde8 <71596914+pshinde8@users.noreply.github.com>
2022-01-17 14:42:18 +0530

    Update Question13.cpp

--- Goldman Sachs/Question13.cpp
+++ Goldman Sachs/Question13.cpp
@@ -1,1 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string decodedString(string s){
+        // code here
+        stack<char> st;
+        string temp1="";
+        string temp2="";
+        string num="";
+        string ans="";
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if (s[i] == ']') 
+            {
+                while (!st.empty() && st.top() != '[') 
+                {
+                    temp1 = st.top() + temp1;
+                    st.pop();
+                }
+                st.pop();
+                while (!st.empty() && isdigit(st.top())) 
+                {
+                    num = st.top() + num;
+                    st.pop();
+                }
+                int number = stoi(num);
+                for (int j = 0; j < number; j++)
+                    temp2 += temp1;
+                for (int i=0; i<temp2.length(); i++)
+                    st.push(temp2[i]);
+            }
+            else
+                st.push(s[i]);
+                
+            temp1="";
+            temp2="";
+            num="";
+        }
+        while (!st.empty()) 
+        {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        Solution ob;
+        cout<<ob.decodedString(s)<<"\n";
+    }
+    return 0;
+} 
