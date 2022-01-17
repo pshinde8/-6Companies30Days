b4eaba802f79a266c19962fba9d4929f130b0f9d
pshinde8 <71596914+pshinde8@users.noreply.github.com>
2022-01-17 14:40:00 +0530

    Update Question12.cpp

--- Goldman Sachs/Question12.cpp
+++ Goldman Sachs/Question12.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution{
+	public:
+		long long int ChessboardSquares(int n)
+		{
+			long long int count = (n*(n+1)*((2*n)+1))/6;
+			return count;
+		}
+};
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		Solution ob;
+		cout<<ob.ChessboardSquares(n)<<endl;
+	}
+	return 0;
+}
