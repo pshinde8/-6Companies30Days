bbba4693b1289594131f7c139ad3195d18634525
pshinde8 <71596914+pshinde8@users.noreply.github.com>
2022-01-17 19:41:36 +0530

    Update Question1.cpp

--- Amazon/Question1.cpp
+++ Amazon/Question1.cpp
@@ -1,44 +1,43 @@
-#include <bits/stdc++.h>
+// Maximum Profit
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxProfit(int K, int N, int A[])
-{
-    // code here
-    int dp[K + 1][N];
-    memset(dp, 0, sizeof(dp));
-    for (int i = 1; i <= K; i++)
+class Solution {
+  public:
+      int maxProfit(int K, int N, int A[])
     {
-        int max_num = INT_MIN;
-        for (int j = 1; j < N; j++)
+        // code here
+        int dp[K+1][N];
+        for(int i = 0; i <= K; i++)
+            dp[i][0] = 0;
+        for(int j = 0; j < N; j++)
+            dp[0][j] = 0;
+        for(int i = 1; i <= K; i++)
         {
-            if ((dp[i - 1][j - 1] - A[j - 1]) > max_num)
+            int maxi = INT_MIN;
+            for(int j = 1; j < N; j++)
             {
-                max_num = dp[i - 1][j - 1] - A[j - 1];
+                maxi = max(maxi, dp[i-1][j-1] - A[j-1]);
+                dp[i][j] = max(maxi + A[j], dp[i][j-1]);
             }
-
-            if ((max_num + A[j]) > dp[i][j - 1])
-                dp[i][j] = max_num + A[j];
-            else
-                dp[i][j] = dp[i][j - 1];
         }
+        return dp[K][N-1];  
     }
+};
 
-    return dp[K][N - 1];
-}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> K;
+        cin >> N;
+        int A[N];
+        for (int i = 0; i < N; i++) cin >> A[i];
 
-int main()
-{
-    int K, N;
-    cin >> K >> N;
-
-    int arr[N];
-    for (int i = 0; i < N; i++)
-        cin >> arr[i];
-
-    int result = maxProfit(K, N, arr);
-
-    cout << result << endl;
-
+        Solution ob;
+        cout << ob.maxProfit(K, N, A) << endl;
+    }
     return 0;
-}
+}  
