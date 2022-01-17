bcee464eec21b1d70e3e177bc3012c560ed9c49f
pshinde8 <71596914+pshinde8@users.noreply.github.com>
2022-01-17 14:13:06 +0530

    Update Question4.cpp

--- Goldman Sachs/Question4.cpp
+++ Goldman Sachs/Question4.cpp
@@ -1,1 +1,47 @@
+// Run Length Encoding
 
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}
+
+
+/*You are required to complete this function */
+
+string encode(string src)
+{     
+  //Your code here 
+  string ans="";
+  //src+='#';
+  int count=0;
+  for(int i=1; i<=src.length(); i++)
+  {
+      if(src[i-1]!=src[i])
+      {
+          count++;
+          ans += src[i-1];
+          ans += to_string(count);
+          count=0;
+      }
+      else
+      {
+          count++;
+      }
+      
+  }
+  return ans;
+} 
